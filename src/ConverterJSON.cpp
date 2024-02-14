ff6aa9ad49bdbeb22bfb0b59af34484123fe91ff
Boris1991 <boris19912204@mail.ru>
2024-02-14 11:37:17 +0600

    Added initial "ConverterJSON" class

---
+++ src/ConverterJSON.cpp
@@ -0,0 +1,26 @@
+/**
+* Метод получения содержимого файлов
+* @return Возвращает список с содержимым файлов перечисленных
+*
+в config.json
+*/
+std::vector<std::string> ConverterJSON::GetTextDocuments() {}
+/**
+* Метод считывает поле max_responses для определения предельного
+* количества ответов на один запрос
+* @return
+*/
+int ConverterJSON::GetResponsesLimit() {}
+/**
+* Метод получения запросов из файла requests.json
+* @return возвращает список запросов из файла requests.json
+*/
+std::vector<std::string> ConverterJSON::GetRequests() {}
+/**
+* Положить в файл answers.json результаты поисковых запросов
+*/
+void ConverterJSON::putAnswers(std::vector<std::vector<std::pair<int, float>>> answers) {}
+
+
+#endif
+
